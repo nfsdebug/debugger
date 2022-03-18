9452436ea2d75eff7e886de0984ab974f9225485
sbstndb <sebastiendubois4@gmail.com>
2022-03-18 16:45:51 +0100

    setup correction
    - Now the setup.sh is working well !!

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.3.tar.gz
-tar -xvzf
+tar -xvzf  ncurses-6.3.tar.gz
 cd ncurses-6.3/
 ./configure --with-pthread --with-shared --with-normal --with-debug
 make all
