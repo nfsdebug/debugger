afbff0bf184ef80f4a41f3f5f9de68da0b3266e5
sbstndbs <sebastien.dubois4@gmail.com>
2022-03-18 10:12:50 +0100

    Bad folder

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.3.tar.gz
 tar -xvzf
-cd ncurses-6.3.tar.gz
+cd ncurses-6.3/
 ./configure --with-pthread --with-shared --with-normal --with-debug
 make all
 sudo make install
