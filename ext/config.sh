4e024148c6d7a597d06fdce53b561b7757d4ddae
sbstndbs <sebastiendubois4@gmail.com>
2022-03-21 00:30:01 +0100

    Ménage

--- ext/config.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/dssgabriel/vec
-cd vec
-make
-make test
--- setup.sh
+++
@@ -1,6 +0,0 @@
-wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.3.tar.gz
-tar -xvzf  ncurses-6.3.tar.gz
-cd ncurses-6.3/
-./configure --with-pthread --with-shared --with-normal --with-debug
-make all
-sudo make install
